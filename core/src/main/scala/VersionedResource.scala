6a49c41f7433e96f0e85b4542a151db84cccf27a
Max Afonov <max@bumnetworks.com>
2013-10-21 10:14:02 -0400

    tail start/stop

--- core/src/main/scala/VersionedResource.scala
+++ core/src/main/scala/VersionedResource.scala
@@ -6,12 +6,13 @@ import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicReference
 
 abstract class VersionedResource[A](tree: Tree, create: Tree => Resource[A], destroy: Resource[A] => Unit = (x: Resource[A]) => ()) {
-  val id = "VersionedResource("+getClass.getSimpleName+" @ "+tree.root+")"
+  val id = "VersionedResource("+tree.root+")"
   val ref = new AtomicReference[Resource[A]](create(tree))
   def map[B](f: A => B): Resource[B] = ref.get().right.map(f)
   val continue = new AtomicReference(true)
   class Tailer(timeout: Long, unit: TimeUnit) extends Runnable {
     val tail = tree.history.tail()
+    tail.start()
     def activate(version: Version) {
       tree.activate(version)
       destroy(ref.getAndSet(create(tree)))
@@ -30,6 +31,7 @@ abstract class VersionedResource[A](tree: Tree, create: Tree => Resource[A], des
           case ie: InterruptedException => {}
         }
       }
+      tail.stop()
     }
   }
   val tailer = new Thread(new Tailer(1000L, TimeUnit.SECONDS), id+"/Tailer")
